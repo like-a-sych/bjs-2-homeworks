9efd9e2d69a4fb0fa2a2ec865c2d7ceed2981968
John Doe <montekke@gmail.com>
2023-01-24 14:24:28 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,14 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let arr = [];
+	let diskriminant = Math.pow(b, 2) - 4 * a * c;
+	if (diskriminant > 0) {
+		let root1 = (-b + Math.sqrt(diskriminant)) / (2 * a);
+		let root2 = (-b - Math.sqrt(diskriminant)) / (2 * a);
+		arr = [root1, root2];
+	} else if (diskriminant === 0) {
+		let root = -b / (2 * a);
+		arr = [root];
+	}
+	return arr; // array
 }
