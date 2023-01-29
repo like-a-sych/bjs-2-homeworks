bcb1d25a4784a7bb71c8a3f4332f70dc9b6571e8
John Doe <montekke@gmail.com>
2023-01-29 15:55:03 +0300

    Первое и второй задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+	return result = arr1.length === arr2.length && arr1.every((element, index) => element == arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (users.length > 0 && (gender == "мужской" || gender == "женский")) {
+		let resultArr = users.filter(function (arr) {
+			return arr.gender == gender;
+		});
+
+		let resultSum = resultArr.reduce(function (sum, num) {
+			return summ = sum + num.age;
+		}, 0)
+		return resultSum / resultArr.length
+	} else {
+		return 0
+	}
 }
