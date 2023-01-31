4f79dcef8db21901315e3ed68f757ed355d1a205
John Doe <montekke@gmail.com>
2023-01-31 13:24:43 +0300

    Дз объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+	this.name = name,
+		this.gender = gender,
+		this.age = age,
+		this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+	if (!(this.marks === undefined)) {
+		this.marks.push(...marksToAdd)
+	}
 }
+
 
 Student.prototype.getAverage = function () {
-  
+	if (this.marks.length > 0) {
+		let summ = this.marks.reduce((sum, elem) => sum + elem);
+		return summ / (this.marks.length)
+	} else {
+		return 0
+	}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason
 }
